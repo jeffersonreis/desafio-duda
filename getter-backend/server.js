bcc25cf13059eaff93c73c2671a62bb7cd76ca2d
jeffersonreis <jeffersonluis.reis@gmail.com>
2025-03-30 22:54:21 -0300

    ajustes

--- getter-backend/server.js
+++ getter-backend/server.js
@@ -8,7 +8,6 @@ app.use(cors());
 
 const API_BASE_URL = 'https://dados.tcerj.tc.br/api/v1';
 
-// Função auxiliar para fazer as requisições
 const makeApiRequest = async (url, params, res) => {
   try {
     const response = await axios.get(url, { params });
@@ -19,31 +18,26 @@ const makeApiRequest = async (url, params, res) => {
   }
 };
 
-// Rota para compras_diretas_estado
 app.get('/api/compras_diretas_estado', (req, res) => {
   const url = `${API_BASE_URL}/compras_diretas_estado`;
   makeApiRequest(url, req.query, res);
 });
 
-// Rota para compras_diretas_municipio
 app.get('/api/compras_diretas_municipio', (req, res) => {
   const url = `${API_BASE_URL}/compras_diretas_municipio`;
   makeApiRequest(url, req.query, res);
 });
 
-// Rota para compras_covid_estado
 app.get('/api/compras_covid_estado', (req, res) => {
   const url = `${API_BASE_URL}/compras_covid_estado`;
   makeApiRequest(url, req.query, res);
 });
 
-// Rota para compras_covid_municipio
 app.get('/api/compras_covid_municipio', (req, res) => {
   const url = `${API_BASE_URL}/compras_covid_municipio`;
   makeApiRequest(url, req.query, res);
 });
 
-// Rota de teste para verificar se o servidor está funcionando
 app.get('/', (req, res) => {
   res.send('Servidor proxy está funcionando!');
 });
